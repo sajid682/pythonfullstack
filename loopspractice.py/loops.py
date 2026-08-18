96e9d6649971f2524c45cfc0f334195c2802b627
Sajid <sajid5541shaik@gmail.com>
2026-08-18 09:53:37 +0530

    loopspractice.py

---
+++ loopspractice.py/loops.py
@@ -0,0 +1,9 @@
+#Print numbers from 1 to 10
+for i in range(1, 11):
+    print(i)
+#Print even numbers from 1 to 50
+for i in range(2, 51, 2):
+    print(i)
+#Print odd numbers from 1 to 50 
+for i in range(1, 51, 2):
+    print(i)
